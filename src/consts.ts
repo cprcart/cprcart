f53c67e77c3c62e2d8a426d3ad1c2e7a3032cd85
cprcart <dtomford0@gmail.com>
2026-03-25 17:15:10 +0530

    Change site title and description for CPR blog
    Updated site title and description to reflect new content.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "CPR Training and Certification Blogs by CPR Cart";
+export const SITE_DESCRIPTION = "CPR Cart’s CPR Training and Certification Blog is your trusted resource for clear, practical, and up-to-date information on lifesaving skills. Designed for healthcare professionals, workplace responders, and everyday individuals, our blog covers everything from BLS, ACLS, and PALS certification guidance to CPR techniques, renewal tips, and emergency response best practices.";
